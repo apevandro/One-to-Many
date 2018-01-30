597f092813cebae7b8fdcb054562d0cf6be99f7b
Evandro Arruda <p000evpereira@prservicos.com.br>
2018-01-30 14:47:26 -0200

    One-to-Many mapping

---
+++ src/main/java/additional_column/unidirectional/Course.java
@@ -0,0 +1,49 @@
+package additional_column.unidirectional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Course {
+
+    private int courseId;
+    private String name;
+    private int totalCredits;
+    private Set<Student> students = new HashSet<Student>();
+
+	public Course() {}
+
+    public Course(int courseId, String name, int totalCredits) {
+    	this.courseId = courseId; 
+        this.name = name;
+        this.totalCredits = totalCredits;
+    }
+
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getTotalCredits() {
+		return totalCredits;
+	}
+
+	public void setTotalCredits(int totalCredits) {
+		this.totalCredits = totalCredits;
+	}
+
+	public Set<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(Set<Student> students) {
+		this.students = students;
+	}
+
+}
---
+++ src/main/java/join_table/bidirectional/Course.java
@@ -0,0 +1,55 @@
+package join_table.bidirectional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Course {
+
+    private int courseId;
+    private String name;
+    private int totalCredits;
+    private Set<Student> students = new HashSet<Student>();
+
+	public Course() {}
+
+    public Course(int courseId, String name, int totalCredits) { 
+        this.courseId = courseId;
+    	this.name = name;
+        this.totalCredits = totalCredits;
+    }
+
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getTotalCredits() {
+		return totalCredits;
+	}
+
+	public void setTotalCredits(int totalCredits) {
+		this.totalCredits = totalCredits;
+	}
+
+	public Set<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(Set<Student> students) {
+		this.students = students;
+	}
+
+	// helper method. It guarantees referential integrity.
+	public void addStudent(Student student) {
+		students.add(student);
+		student.setCourse(this);
+	}
+
+}
